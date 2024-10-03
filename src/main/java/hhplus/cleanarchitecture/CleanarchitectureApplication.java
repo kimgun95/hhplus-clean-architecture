ab9290a3c4f87da93afd018f93a49dc2add00b1e
kimgun95 <harrison1995@naver.com>
2024-10-03 11:08:00 +0900

    init: 2주차 프로젝트 초기화

---
+++ src/main/java/hhplus/cleanarchitecture/CleanarchitectureApplication.java
@@ -0,0 +1,13 @@
+package hhplus.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchitectureApplication.class, args);
+	}
+
+}
